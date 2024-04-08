20dd9567c8660caa6eddd1cf0b6a53c64cac1a44
2627067561 <2627067561@qq.com>
2024-04-08 15:05:30 +0000

    main

---
+++ mach-info/config.h
@@ -0,0 +1,4 @@
+#define BUILT_TIME "2024-04-08 14:53:10"
+#define CC_VERSION "cc (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+#define CXX_VERSION "c++ (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+#define SYSTEM_TYPE "GNU/Linux x86_64"
